18a0722ce8889c250c84523301a2b0e13f58ab40
Haitham <756124@stu.ucenmanchester.ac.uk>
2021-07-12 15:03:00 +0100

    shopping basket experiment success

--- js/app.js
+++ js/app.js
@@ -2,12 +2,30 @@ $(function () {
   $('.btn-outline-success').click(function (e) {
     const product = $(this).data();
     console.log(product)
+
+    //get the element document.getElementById('cartTotal');
+    //get the innerHTML content. 
+    //remove the £ sign so you can read it like a number
+    //add the new item price to the value above
+    //set the innerHTML of cartTotal to the new value. 
+    
+    const itemTemplate = document.getElementById('basketTemplate');
+    if(itemTemplate) {
+      const basketItem = itemTemplate.children[0].cloneNode(true);
+      const firstRow = basketItem.children[0].children[0]
+      const thirdRow = basketItem.children[0].children[2]
+      firstRow.innerHTML = product.title
+      thirdRow.innerHTML = "£" + product.price
+      console.log(basketItem)
+      const shoppBasket = document.getElementById('shoppingBasket')
+      shoppBasket.appendChild(basketItem)
+    }
   });
 })
-$(document).ready(function () {
-  $('.mdb-select').materialSelect();
-  $('.select-wrapper.md-form.md-outline input.select-dropdown').bind('focus blur', function () {
-    $(this).closest('.select-outline').find('label').toggleClass('active');
-    $(this).closest('.select-outline').find('.caret').toggleClass('active');
-  });
-});
+// $(document).ready(function () {
+//   $('.mdb-select').materialSelect();
+//   $('.select-wrapper.md-form.md-outline input.select-dropdown').bind('focus blur', function () {
+//     $(this).closest('.select-outline').find('label').toggleClass('active');
+//     $(this).closest('.select-outline').find('.caret').toggleClass('active');
+//   });
+// });
