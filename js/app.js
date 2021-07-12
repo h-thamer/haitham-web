c4f1d6a481a7d22e7db15d825aae153d6433e09e
Haitham <756124@stu.ucenmanchester.ac.uk>
2021-07-12 14:03:59 +0100

    changes up to 12/07

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.btn-outline-success').click(function (e) {
+    const product = $(this).data();
+    console.log(product)
+  });
+})
+$(document).ready(function () {
+  $('.mdb-select').materialSelect();
+  $('.select-wrapper.md-form.md-outline input.select-dropdown').bind('focus blur', function () {
+    $(this).closest('.select-outline').find('label').toggleClass('active');
+    $(this).closest('.select-outline').find('.caret').toggleClass('active');
+  });
+});
